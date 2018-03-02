91827fc2fe22c06ac2fe9441fb6671cb132a2424
GUNROX95 <shkulant95@mail.ru>
2018-03-02 18:03:30 +0300

    fast fix for SM.ml

--- src/SM.ml
+++ src/SM.ml
@@ -61,14 +61,6 @@ let rec comp_ex ex =
 
 (* Top-level evaluation
 
-     val run : int list -> prg -> int list
-
-   Takes an input stream, a program, and returns an output stream this program calculates
-*)
-let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
-
-(* Top-level evaluation
-
      val run : prg -> int list -> int list
 
    Takes an input stream, a program, and returns an output stream this program calculates
