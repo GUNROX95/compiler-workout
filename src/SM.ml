1d4789e10674b9afe7a971be25eb3d87fcccaa47
GUNROX95 <shkulant95@mail.ru>
2018-04-03 20:43:16 +0300

    hw5

--- src/SM.ml
+++ src/SM.ml
@@ -28,7 +28,24 @@ type config = int list * Stmt.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                         
-let rec eval env conf prog = failwith "Not yet implemented"
+let rec eval env ((stack, ((st, i, o) as c)) as conf) = function
+| [] -> conf
+| insn :: prg' ->
+  match insn with
+  | BINOP op -> let y::x::stack' = stack in eval env (Expr.to_func op x y :: stack', c) prg'
+  | READ     -> let z::i'        = i     in eval env (z::stack, (st, i', o)) prg'
+  | WRITE    -> let z::stack'    = stack in eval env (stack', (st, i, o @ [z])) prg'
+  | CONST i  -> eval env (i::stack, c) prg'
+  | LD x     -> eval env (st x :: stack, c) prg'
+  | ST x     -> let z::stack'    = stack in eval env (stack', (Expr.update x z st, i, o)) prg'
+  | LABEL _ -> eval env (stack, c) prg'
+  | JMP label -> eval env (stack, c) (env#labeled label)
+  | CJMP (cond, label)  -> 
+    let z::stack' = stack in 
+    let x = match cond with
+    | "nz" -> z <> 0
+    | "z" -> z = 0 in
+    eval env (stack', c) (if (x) then (env#labeled label) else prg')
 
 (* Top-level evaluation
 
@@ -53,4 +70,38 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile p = failwith "Not yet implemented"
+let env = object 
+  val mutable id = 0
+  method next_lbl = 
+    id <- (id + 1);
+    "L" ^ string_of_int id
+end
+
+let rec compile =
+  let rec expr = function
+  | Expr.Var   x          -> [LD x]
+  | Expr.Const n          -> [CONST n]
+  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  in
+  function
+  | Stmt.Seq (s1, s2)  -> compile s1 @ compile s2
+  | Stmt.Read x        -> [READ; ST x]
+  | Stmt.Write e       -> expr e @ [WRITE]
+  | Stmt.Assign (x, e) -> expr e @ [ST x]
+  | Stmt.Skip -> []
+  | Stmt.If (e, s1, s2) ->
+    let else_lbl = env#next_lbl in
+    let end_lbl = env#next_lbl in
+    let curr = compile s1 in
+    let last = compile s2 in
+    (expr e @ [CJMP ("z", else_lbl)] @ curr @ [JMP end_lbl] @ [LABEL else_lbl] @ last @ [LABEL end_lbl])
+  | Stmt.While (e, s) ->
+    let end_lbl = env#next_lbl in
+    let loop_lbl = env#next_lbl in
+    let body = compile s in
+    ([JMP end_lbl] @ [LABEL loop_lbl] @ body @ [LABEL end_lbl] @ expr e @ [CJMP ("nz", loop_lbl)])
+  | Stmt.Repeat (e, s) ->
+    let loop_lbl = env#next_lbl in
+    let body = compile s in
+    ([LABEL loop_lbl] @ body @ expr e @ [CJMP ("z", loop_lbl)])
+
